bb806a860ad8e1962fc87362f1a9e506a38f712f
Maryam Naji <maryamnaji.mn@gmail.com>
2019-12-22 16:36:38 -0500

    Change xml to json formatter

--- IdeaApp/IdeaApp.API/App_Start/WebApiConfig.cs
+++ IdeaApp/IdeaApp.API/App_Start/WebApiConfig.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Web.Http;
 
 namespace IdeaApp.API
@@ -10,13 +11,15 @@ namespace IdeaApp.API
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
+            config.Formatters.JsonFormatter.SupportedMediaTypes
+                .Add(new MediaTypeHeaderValue("text/html"));
 
             // Web API routes
             config.MapHttpAttributeRoutes();
 
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
+                routeTemplate: "api/{controller}/{action}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
         }
